500aad786930b96b225b2bd5dddf23ff1c56dbe7
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-29 22:47:47 +0000

    Added Review Model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from books.models import Book
 
 # Create your models here.
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.user.username} - {self.book.title}'
