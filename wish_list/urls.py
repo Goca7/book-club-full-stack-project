d6f8949646067574270ecb6a4f58047b07f179d0
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 03:23:23 +0000

    Created wish_list/url.py

---
+++ wish_list/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views  # Import views from the wish_list app
+
+urlpattern = [
+    # URL for the wish list page
+    path('', views.wish_list_view, name='wish_list_view'),
+
+]
