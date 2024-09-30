381e793337a390bfe5c433fd8f55b9cf469b5a18
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 18:53:51 +0000

    Added CRUD functions for the Review model

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from .forms import BookForm
 from reviews.models import Review
 from reviews.forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
 
 # View to display a list of all books
@@ -20,15 +21,24 @@ def book_details_view(request, slug):
     # Fetch all reviews for the book from the database
     reviews = Review.objects.filter(book=book)
 
-    # Handle review form submission
-    if request.method == 'POST':
+    # Handle review form submission a review form
+    if 'add_review' in request.'POST':
         reviews_form = ReviewForm(request.POST)
         if reviews_form.is_valid():
             # Save the review to the database
-            new_review = reviews_form.save(commit=False)
+            new_review = review_form.save(commit=False)
             new_review.book = book  # Associate the review with the book
             new_review.user = request.user  # Associate the review with the logged-in user
             new_review.save()
+            return redirect('book_details_view', slug=book.slug)
+    # Handle review update
+    elif 'edit_review' in request.POST:
+        review_id = request.POST.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        reviews_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            # Save the updated review to the database
+            reviews_form.save()
             return redirect('book_details_view', slug=book.slug)
     else:
         # Create a blank review form
@@ -38,9 +48,28 @@ def book_details_view(request, slug):
     context = {
         'book': book,
         'reviews': reviews,
-        'reviews_form': reviews_form,  # Form for adding new reviews to the book
+        'review_form': review_form,  # Form for adding/editing reviews
     }
     return render(request, 'books/book_detail.html', context)
+
+    # Delete a review (only the review owner or a superuser can delete)
+
+    @login_required
+    def delete_review(request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+
+    # Check if the user is either the review owner or a superuser
+    if request.user == review.user or request.user.is_superuser:
+        review.delete()
+        # Success message
+        messages.success(request, "Review deleted successfully.")
+    else:
+        # If the user is neither the review owner nor a superuser, add an error message
+        messages.error(
+            request, "You do not have permission to delete this review.")
+
+    # Redirect back to the book's detail page with the slug
+    return redirect('book_details_view', slug=book.slug)
 
 
 # View to create a new book
