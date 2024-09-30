69a0a821b577b1172afb36c902d2dc5e9dd134a0
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 00:17:55 +0000

    Created a new SECRET_KEY

--- book_club/settings.py
+++ book_club/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-beacsxq(-37h@$8u4)h7dtp0p4s_e*_s)8wm+mt$g*f@+6ez%)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
