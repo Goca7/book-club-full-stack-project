2c58db3ad71b2e7fb3e58aa3e8e68f132eab67bf
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 13:23:49 +0000

    Reordered the URL patterns in books/urls.py

--- books/urls.py
+++ books/urls.py
@@ -2,18 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URL for the book list (Read list of all books)
-    path('', views.books_view, name='books_view'),
-
-    # URL for a single book (Read a single book's details)
-    path('<slug:slug>/', views.book_details_view, name='book_details_view'),
-
     # URL for creating a new book (Create a new book)
     path('create/', views.create_book, name='create_book'),
+
+    # URL for the book list (Read list of all books)
+    path('', views.books_view, name='books_view'),
 
     # URL for updating an existing book (Update a book's details)
     path('<slug:slug>/update/', views.update_book, name='update_book'),
 
     # URL for deleting an existing book (Delete a book)
     path('<slug:slug>/delete/', views.delete_book, name='delete_book'),
+
+    # URL for a single book (Read a single book's details)
+    path('<slug:slug>/', views.book_details_view, name='book_details_view'),
+
 ]
