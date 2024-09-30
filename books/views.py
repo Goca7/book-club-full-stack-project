cd0aca7f4d6e334142f5f1b2c520a796cb9d372f
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 11:42:33 +0000

    Created Update Book View and Delete Book View in books/views.py

--- books/views.py
+++ books/views.py
@@ -6,22 +6,19 @@ from .forms import BookForm
 # View to display a list of all books
 def books_view(request):
     books = Book.objects.all()  # Fetch all books from the database
-    context = {'books': books}  # Pass the books to the template
     # Render the template with the books
-    return render(request, 'books/book_list.html', context)
+    return render(request, 'books/book_list.html', {'books: books})'})
 
 
 # View to display a single book using its slug
 def book_details_view(request, slug):
     # Fetch the book from the database using its slug
-    book = get_object_or_404(Book, slug=slug)
-    context = {'book': book}  # Pass the book to the template
+    book = Book.objects.get(slug=slug)
     # Render the template with the book details
-    return render(request, 'books/book_detail.html', context)
+    return render(request, 'books/book_detail.html', {book: book})
+
 
 # View to create a new book
-
-
 def create_book(request):
     if request.method == 'POST':
         # Create a form object using the POST data
@@ -36,3 +33,27 @@ def create_book(request):
         form = BookForm()
     # Render the template with the form for creating a new book
     return render(request, 'books/create_book.html', {'form': form})
+
+
+# View to update an existing book
+def update_book(request, slug):
+    book = Book.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            # Redirect to updated book details
+            return redirect('book_details_view', slug=book.slug)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/update_book.html', {'form': form})
+
+# View to delete a book
+
+
+def delete_book(request, slug):
+    book = Book.objects.get(slug=slug)
+    if request.method == 'POST':
+        book.delete()  # Delete the book from the database
+        return redirect('books_view')  # Redirect to the book list page
+    return render(request, 'books/delete_book.html', {'book': book})
