39e9b0c6ef42ee24193f6cdf373e7a991049df87
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 02:23:55 +0000

    Created centralised templates directory; updated books/views.py; updated settings.py

--- books/views.py
+++ books/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Book
-
-# Create your views here.
 
 
 # View to display a list of all books
@@ -10,7 +7,7 @@ def books_view(request):
     books = Book.objects.all()  # Fetch all books from the database
     context = {'books': books}  # Pass the books to the template
     # Render the template with the books
-    return render(request, 'book_list.html', context)
+    return render(request, 'books/book_list.html', context)
 
 # View to display a single book using its slug
 
@@ -20,4 +17,4 @@ def book_details_view(request, slug):
     book = get_object_or_404(Book, slug=slug)
     context = {'book': book}  # Pass the book to the template
     # Render the template with the book details
-    return render(request, 'book_detail.html', context)
+    return render(request, 'books/book_detail.html', context)
