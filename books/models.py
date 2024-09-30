44281c63721e5b8ca52d7bd8ad6e1b5b4af5d7e7
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 01:44:36 +0000

    Set up the views for listing books and showing details for individual books using slugs

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,6 +9,14 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     genre = models.TextField()
     description = models.TextField()
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # Generate a unique slug for the book based on its title.
+        if not self.slug:
+            self.slug = slugify(self.title)
+        # Call the real save() method to save to the database.
+        super(Book, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- books/views.py
+++ books/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
 
+# View to display a list of all books
 def books_view(request):
-    return HttpResponse("Hello, this is all about books!")
+    books = Book.objects.all()  # Fetch all books from the database
+    context = {'books': books}  # Pass the books to the template
+    # Render the template with the books
+    return render(request, 'book_list.html', context)
+
+# View to display a single book using its slug
+
+
+def book_details_view(request, slug):
+    # Fetch the book from the database using its slug
+    book = get_object_or_404(Book, slug=slug)
+    context = {'book': book}  # Pass the book to the template
+    # Render the template with the book details
+    return render(request, 'book_detail.html', context)
