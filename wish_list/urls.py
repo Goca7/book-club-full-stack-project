61166e633139bad9cbd37564bcf222f4d953eb2a
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 07:40:28 +0000

    Corrected a typo in wish_list/urls.py, line 4

--- wish_list/urls.py
+++ wish_list/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views  # Import views from the wish_list app
 
-urlpattern = [
+urlpatterns = [
     # URL for the wish list page
     path('', views.wish_list_view, name='wish_list_view'),
 
