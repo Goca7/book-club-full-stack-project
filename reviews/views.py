b1c1480894673089d983ab61306db135dbf6618d
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-30 14:43:42 +0000

    Removed unused reviews_view

--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-
-def reviews_view(request):
-    return HttpResponse("Hello, this is all about reviews!")
