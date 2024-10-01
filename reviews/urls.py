4ec2766f3cb35a75462393f68ce7a692105100e4
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-01 08:27:32 +0000

    Fixed reviews/views.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views  # Import views from the reviews app
 
 urlpatterns = [
-    # List all reviews
-    path('', views.reviews_view, name='reviews_view'),
+    path('', views.show_reviews, name='reviews_view'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+ef reviews_view(request):
+    # Example logic for displaying reviews
+    return render(request, 'reviews/reviews_list.html', {})
