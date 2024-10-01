1511c083de9f4fe796dfbdc4b4116cc3255bce81
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-01 08:29:17 +0000

    Fixed a typo

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-ef reviews_view(request):
+
+
+def reviews_view(request):
     # Example logic for displaying reviews
     return render(request, 'reviews/reviews_list.html', {})
