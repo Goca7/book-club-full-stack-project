a702b722b7526dcf102562cced7b8469b77d944d
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-01 16:25:37 +0000

    Fix template directory structure and path for home page

--- book_club/settings.py
+++ book_club/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'book_club.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
