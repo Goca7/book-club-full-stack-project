ffe5b85b3cf66f1ffb2b9a267b6de05d44ee17ac
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-02 01:52:39 +0000

    Created Read function in wish_list app

--- wish_list/views.py
+++ wish_list/views.py
@@ -5,9 +5,8 @@ from books.models import Book
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 # Create a new entry in the "Want-to-read" list
-
-
 def add_to_wish_list(request, book_id):
     if request.user.is_authenticated:
         # Fetch the book with the given ID and create a new entry in the WishList model
@@ -20,6 +19,22 @@ def add_to_wish_list(request, book_id):
         else:
             # Redirect to the book details page if the book is already in the Wish List
             return redirect('book_details_view', slug=book.slug)
+    else:
+        # Redirect to the login page if the user is not authenticated
+        return redirect('login')
+
+# Display the "Want-to-read" list of books for the current user
+
+
+def wish_list_view(request):
+    if request.user.is_authenticated:
+        # Fetch all books from the WishList model associated with the current user
+        user_books = WishList.objects.filter(user=request.user)
+        context = {
+            'user_books': user_books
+        }
+        # Render the template with the books in the wish list
+        return render(request, 'wish_list/wish_list.html', context)
     else:
         # Redirect to the login page if the user is not authenticated
         return redirect('login')
