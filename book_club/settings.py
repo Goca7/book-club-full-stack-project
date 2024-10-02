bec1b67cede5fd8ccf22f1dae8fddfad42086d22
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-02 13:57:43 +0000

    Configured media settings

--- book_club/settings.py
+++ book_club/settings.py
@@ -147,3 +147,7 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
