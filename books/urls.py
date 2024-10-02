383c6f48ea6331def9803b5ef20fc64e43518972
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-02 11:42:02 +0000

    Set up URL for Want-to-read button

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('create/', views.create_book, name='create_book'),
     path('list/', views.books_view, name='books_view'),  # Book listing page
+    path('<int:book_id>/add-to-wish-list/', views.add_to_wish_list_from_list,
+         name='add_to_wish_list_from_list'),  # Add to wish list
     path('<slug:slug>/update/', views.update_book, name='update_book'),
     path('<slug:slug>/delete/', views.delete_book, name='delete_book'),
     path('detail/<slug:slug>/', views.book_details_view, name='book_details_view'),
