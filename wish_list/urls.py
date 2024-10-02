ee3521cb2f0cabef6bc98b0f3ca84ac094655616
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-02 02:05:05 +0000

    Updated wish_list/urls.py

--- wish_list/urls.py
+++ wish_list/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views  # Import views from the wish_list app
 
 urlpatterns = [
-    # URL for the wish list page
+    # URL for displaying the wish list
     path('', views.wish_list_view, name='wish_list_view'),
+
     # URL for the signup page
     path('signup/', views.signup, name='signup'),
 
+    # URL for adding a book to the wish list
+    path('add/<int:book_id>/', views.add_to_wish_list, name='add_to_wish_list'),
+
+    # URL for removing a book from the wish list
+    path('remove/<int:wish_list_id>/', views.remove_from_wish_list,
+         name='remove_from_wish_list'),
 ]
